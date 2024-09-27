f7004bb4fcf42c6da6e7a02087052feeae74a265
Mavic Lamela Irureta <[mavic_81@hotmail.com]>
2024-09-27 10:39:33 +0200

    Ejercicio Resuelto

--- src/main/java/com/campusdual/classroom/Exercise03.java
+++ src/main/java/com/campusdual/classroom/Exercise03.java
@@ -1,27 +1,42 @@
 package com.campusdual.classroom;
 
+import java.util.Objects;
+
 public class Exercise03 {
 
     // Esto es una funcion, ya que devuelve un valor
-    // TODO ↓: Tiene que devolver el siguiente mensaje: "Hola. Bienvenido " y la cadena de textos que se le pasa por parámetro.
+
     public static String greetings(String name) {
-        return null;
+        String mensajeBienvenida = "Hola. Bienvenido " + name;
+        return mensajeBienvenida;
     }
 
     // Esto es una funcion, ya que devuelve un valor
-    // TODO ↓: Tiene que devolver el siguiente mensaje: "Ups. No pudimos validar tus datos. Tu nombre de usuario no es " y la cadena de texto que se le pasa por parámetro
+
     public static String error(String nombre) {
-        return null;
+        String mensajeError = "Ups. No pudimos validar tus datos. Tu nombre de usuario no es " + nombre;
+
+        return mensajeError;
     }
 
     //Esto es un procedimiento, ya que no devuelve un valor
-    // TODO ↓: Tiene que comprobar si el usuario que recibe por parámetro es "Sebas" y su contraseña es "sebas01". Si es correcto, que muestre el mensaje de la función greetings() y si no que muestre el mensaje de la función error()
-    public static void checkUser(String user, String pass) {
 
+    public static void checkUser(String user, String pass) {
+        String nombreCorrecto = "Sebas";
+        String contraseñaCorrescta = "sebas01";
+
+        if (Objects.equals(nombreCorrecto, user) && Objects.equals(contraseñaCorrescta, pass)) {
+            System.out.println(greetings(user));
+        } else {
+            System.out.println(error(nombreCorrecto));
+        }
     }
 
 
     public static void main(String[] args) {
+        checkUser("Sebas", "sebas01");
+        checkUser("Mavic", "Mavic01");
+
 
     }
 }
